088d7b90b1775654d4853902375d1abab2a4cc31
DellanX <DellanX@Live.com>
2024-01-09 13:27:45 -0600

    Add Comments to Reference Types

--- src/types/references/method-reference-status.ts
+++ src/types/references/method-reference-status.ts
@@ -3,7 +3,18 @@ import { ExecutionStatus } from '../execution-status';
 import { Ref } from 'vue';
 
 export type MethodReferenceStatus<TResult, TReference extends MethodReference<TResponse, TArgs>, TResponse, TArgs extends any[], TError extends Error = Error> = TReference & {
+    /**
+     * Contains an Error if an Error has occurred.
+     * Null otherwise.
+     */
     error: Ref<TError | null>;
+    /**
+     * Contains the result, if successful.
+     * Null otherwise.
+     */
     result: Ref<TResult | null>;
+    /**
+     * Contains status information about the execution.
+     */
     status: Ref<ExecutionStatus | null>;
 }
--- src/types/references/method-reference.ts
+++ src/types/references/method-reference.ts
@@ -3,10 +3,28 @@ import { MethodStage } from '../method-stage';
 import { ComputedRef } from "vue"
 
 export type MethodReference<TResponse, TArgs extends any[]> = {
+    /**
+     * The time the last execution completed at
+     */
     lastExecuted: ComputedRef<Date>;
+    /** 
+     * A method to trigger the execution
+     */
     execute: Method<TResponse, TArgs>;
+    /** 
+     * Is the method currently executing?
+     */
     executing: ComputedRef<boolean>;
+    /** 
+     * Has this method already completed an execution?
+     */
     executed: ComputedRef<boolean>;
-    response: ComputedRef<TResponse>;
+    /**
+     * The result from the completed execution. Null if not yet executed
+     */
+    response: ComputedRef<TResponse|null>;
+    /**
+     * Combines the execution and executed variables into an enum.
+     */
     stage: ComputedRef<MethodStage>;
 }
